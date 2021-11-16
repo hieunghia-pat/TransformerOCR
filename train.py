9ce0987e1d65ef2138ceefb0fa8aa9652c7424d9
hieunghia-pat <19520178@gm.uit.edu.vn>
2021-11-16 18:02:36 +0700

    added evaluation process

--- train.py
+++ train.py
@@ -1,12 +1,13 @@
 import pickle
 import torch
+from torch.utils.data.dataloader import DataLoader
 from metric_utils.metrics import Metrics
 from metric_utils.tracker import Tracker
 from data_utils.vocab import Vocab
 from model.transformer import make_model
 import os
 from loss_utils.LabelSmoothingLoss import LabelSmoothing, SimpleLossCompute, NoamOpt
-from data_utils.dataloader import Batch, OCRDataset
+from data_utils.dataloader import Batch, OCRDataset, collate_fn
 from tqdm import tqdm
 
 import config
@@ -57,7 +58,8 @@ def train():
     else:
         vocab = pickle.load(open(f"vocab_{config.out_level}.pkl", "rb"))
 
-    dataset = OCRDataset(dir=os.path.join(config.image_dir, "train_data"), image_size=(-1, 64), out_level=config.out_level, vocab=vocab)
+    train_dataset = OCRDataset(dir=os.path.join(config.image_dir, "train_data"), image_size=config.image_size, out_level=config.out_level, vocab=vocab)
+    test_dataset = OCRDataset(dir=os.path.join(config.image_dir, "test_data"), image_size=config.image_size, out_level=config.out_level, vocab=vocab)
     metric = Metrics(vocab)
     tracker = Tracker()
     
@@ -71,27 +73,50 @@ def train():
     model_opt = NoamOpt(model.tgt_embed[0].d_model, 1, 2000,
             torch.optim.Adam(model.parameters(), lr=config.learning_rate, betas=(0.9, 0.98), eps=1e-9))
 
-    folds = dataset.get_folds()
+    folds = train_dataset.get_folds()
+    test_dataloder = DataLoader(test_dataset, 
+                                batch_size=config.batch_size, 
+                                shuffle=True, 
+                                collate_fn=collate_fn)
     for stage in range(len(folds)):
-        best_score = {
+        best_scores = {
                 "cer": 0,
                 "wer": 0
+        }
+
+        scores_on_test = {
+            "cer": 0,
+            "wer": 0
         }
 
         for epoch in range(config.max_epoch):
             run_epoch(folds[:-1], True, "Training", epoch, model, 
                 SimpleLossCompute(model.generator, criterion, model_opt), metric, tracker)
-            scores = run_epoch([folds[-1]], False, "Evaluation", epoch, model, 
+            val_scores = run_epoch([folds[-1]], False, "Validation", epoch, model, 
+                SimpleLossCompute(model.generator, criterion, None), metric, tracker)
+            test_scores = run_epoch([folds[-1]], False, "Evaluation", epoch, model, 
                 SimpleLossCompute(model.generator, criterion, None), metric, tracker)
 
-            if best_score["cer"] < scores["cer"]:
-                best_score["cer"] = scores["cer"]
-                best_score["wer"] = scores["wer"]
-                torch.save(os.path.join(config.checkpoint_path, f"best_model_stage_{stage+1}.pth"))
+            if best_scores["cer"] < val_scores["cer"]:
+                best_scores["cer"] = val_scores["cer"]
+                best_scores["wer"] = val_scores["wer"]
+                scores_on_test = test_scores
+                torch.save({
+                    "state_dict": model.state_dict(),
+                    "val_scores": val_scores,
+                    "test_scores": test_scores,
+                }, os.path.join(config.checkpoint_path, f"best_model_stage_{stage+1}.pth"))
 
-            torch.save(os.path.join(config.checkpoint_path, f"last_model_stage_{stage+1}.pth"))
+            torch.save({
+                "state_dict": model.state_dict(),
+                "val_scores": val_scores,
+                "test_scores": test_scores,
+            }, os.path.join(config.checkpoint_path, f"last_model_stage_{stage+1}.pth"))
 
-            print("+"*13)
+            print("*"*13)
+
+        print(f"Stage {stage+1} completed. Scores on test set: CER = {scores_on_test['cer']} - WER = {scores_on_test['wer']}")
+        print("="*23)
 
         # swapping folds
         for idx in range(len(folds)):
