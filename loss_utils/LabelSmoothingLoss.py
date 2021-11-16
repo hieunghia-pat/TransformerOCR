0ad0501e2a914c62fe8b169ae681cc052a1e70f8
hieunghia-pat <19520178@gm.uit.edu.vn>
2021-11-16 21:01:44 +0700

    no reduction on computing loss

--- loss_utils/LabelSmoothingLoss.py
+++ loss_utils/LabelSmoothingLoss.py
@@ -32,7 +32,7 @@ class LabelSmoothing(nn.Module):
     "Implement label smoothing."
     def __init__(self, size, padding_idx=0, smoothing=0.0):
         super(LabelSmoothing, self).__init__()
-        self.criterion = nn.KLDivLoss(reduction="mean")
+        self.criterion = nn.KLDivLoss(reduction=None)
         self.padding_idx = padding_idx
         self.confidence = 1.0 - smoothing
         self.smoothing = smoothing
